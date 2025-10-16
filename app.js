78fffd05977cd7395d17ed7fdfa6ec4e0dca7d01
Gustavo Moreira Bastos <113913787+Moreira1214@users.noreply.github.com>
2025-10-16 19:20:05 -0300

    Update app.js
    Mudei o numero limite

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do numero secreto');
-    exibirTextoNaTela('p', 'Escolha um numero de 1 a 10');
+    exibirTextoNaTela('p', `Escolha um numero de 1 a ${numeroLimite}`);
 }
 
 exibirMensagemInicial()
@@ -67,4 +67,5 @@ function reiniciarJogo() {
 }
 
 // lista.push('elemento') adiciona
+
 // lista.pop() remove o ultimo elemento na lista
